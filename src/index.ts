473baf8b6a627694dd970144a715d766bccbc8bb
blaiseee <mynameisblaiseflorendo@gmail.com>
2025-10-14 02:29:33 +0800

    main logic

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,21 @@ import router from './routes/profile.route';
 import cookieParser from 'cookie-parser';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || [
-      'http://localhost:5173',
+    origin: [
+      // Frontend URL
+      process.env.FRONTEND_URL || 'http://localhost:5173',
+      // API Gateway URL
+      process.env.API_GATEWAY_URL || 'http://localhost:3000',
+      // DevTunnels pattern
       /https:\/\/.*\.devtunnels\.ms\/?$/,
+      'http://localhost:5173',
+      'http://localhost:3000',
     ],
     credentials: true,
   })
