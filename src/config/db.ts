ccc9563ba363fe0b3978e383713750ac5959d397
blaiseee <mynameisblaiseflorendo@gmail.com>
2025-10-18 17:34:41 +0800

    db refactor

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,18 +1,18 @@
-import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-
-dotenv.config();
-
-const uri = process.env.MONGO_URI;
 
 export const connectDB = async() => {
   try {
-    const DB_URI = uri;
+    const DB_URI = process.env.MONGO_URI;
     if (!DB_URI) {
       throw new Error('DB_URI is noted defined in the environment variables.');
     }
 
-    await mongoose.connect(DB_URI);
+    await mongoose.connect(DB_URI, {
+      maxPoolSize: 10,
+      minPoolSize: 2,
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 30000,
+    });
     console.log('Connected to database');
   } catch (error: any) {
     console.log(error.message);
